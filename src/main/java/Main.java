21dcd5d644db77e274afd8aa5e35c75b4bef4511
Iman Setya Adji <v-iman.setya@anteraja.id>
2021-11-21 04:47:13 +0700

    fix: change crawl behavior, map crawl data into csv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,30 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import Model.Item;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.FileWriter;
+import java.io.Writer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     private static final String baseUrl ="https://www.tokopedia.com/p/handphone-tablet/handphone";
-    private static final String csvPath ="C:output.csv";
+    private static final String csvPath ="D:\\output.csv";
+    private static  final String webDriverPath ="C:\\WebDriver\\gecko\\bin\\geckodriver.exe";
     public static void main(String[] args) {
         //Chrome Setup
 //        System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
@@ -36,51 +46,107 @@ public class Main {
         //Firefox headless setup
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         //firefoxOptions.setHeadless(true);
+
         //Set Path of Firefox driver
-        System.setProperty("webdriver.gecko.driver", "C:\\WebDriver\\gecko\\bin\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", webDriverPath);
         WebDriver driver = new FirefoxDriver(firefoxOptions);
 
         //load web page
         int numPage = 2;
+        List<Item> itemList = new ArrayList<Item>();
         for(int i=1;i<=numPage;i++){
-            crawl(driver, i);
+            itemList.addAll(crawl(driver, i));
         }
-
         driver.close();
+
+        //to CSV
+        Path paths = Paths.get(csvPath);
+        try {
+            writeCsvFromBean(paths,itemList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //to JSON
+//        ObjectMapper mapper = new ObjectMapper();
+////		mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+//        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+//        mapper.enable(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES);
+//        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//        String jsonResult = null;
+//        try {
+//            jsonResult = mapper.writeValueAsString(itemList);
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(jsonResult);
 
     }
 
-    public static void crawl(WebDriver driver,int page){
+    public static List<Item> crawl(WebDriver driver,int page){
+        //fetch web page
         driver.get(baseUrl+"?page="+page);
 
         JavascriptExecutor js = (JavascriptExecutor) driver;
         //scroll browser to trigger onload data
-        js.executeScript("window.scrollBy(0,1000)");
+        js.executeScript("window.scrollBy(0,1030)");
 
         //wait until all item loaded this page
-        List<WebElement> result = new WebDriverWait(driver, Duration.ofSeconds(20))
-                .until(ExpectedConditions.numberOfElementsToBe(By.xpath("//div[@class='css-16vw0vn' and div[@class='css-11s9vse' and not(div[@class='css-nysll7'])]]"),60));
-
-//        List<WebElement> listBarang =driver.findElements(By.xpath("//a[@class='css-89jnbj' and not(div[@class='css-nysll7'])]"));
-//        List<WebElement> listBarang =driver.findElements(By.xpath("//div[@class='css-16vw0vn' and div[@class='css-11s9vse' and not(div[@class='css-nysll7'])]]"));\
+        List<WebElement> result =null;
+        try {
+            result= new WebDriverWait(driver, Duration.ofSeconds(10))
+                    .until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//div[@class='css-16vw0vn' and div[@class='css-11s9vse' and not(div[@class='css-nysll7'])]]"),10));
+        }catch (TimeoutException e){
+            e.printStackTrace();
+        }
 
         //Mapping data item
         System.out.println(result.size());
         int idx =0;
+        List<Item> itemList = new ArrayList<Item>();
         for(WebElement webElement:result){
-            System.out.print(++idx);
+            Item item = new Item();
+            System.out.println(++idx);
 
             //wait until item picture fully loaded
-            Boolean imageResult = new WebDriverWait(driver, Duration.ofSeconds(30))
-                    .until(ExpectedConditions.attributeContains(webElement.findElement(By.tagName("img")),"class","success"));
-            String imageUrl =webElement.findElement(By.tagName("img")).getAttribute("src");
-            System.out.println(imageUrl);
+            try {
+                Boolean imageResult = new WebDriverWait(driver, Duration.ofSeconds(10))
+                        .until(ExpectedConditions.attributeContains(webElement.findElement(By.tagName("img")),"class","success"));
+            }catch (TimeoutException e){
+                e.printStackTrace();
+            }
+
+            //System.out.println(webElement.getText());
+            item.setItemName(webElement.findElement(By.cssSelector(".css-1bjwylw")).getText());
+            item.setPrice(webElement.findElement(By.cssSelector(".css-o5uqvq")).getText());
+            String shopDetail[] = webElement.findElement(By.cssSelector(".css-vbihp9")).getAttribute("innerText").split("\\r?\\n");
+            item.setSellerLocation(shopDetail[0]);
+            item.setSellerName(shopDetail[1]);
+            WebElement ratingElement =webElement.findElement(By.className("css-11s9vse")).findElement(By.className("css-153qjw7")).findElement(By.tagName("div"));
+            item.setRating(ratingElement.findElements(By.cssSelector("img[src='https://assets.tokopedia.net/assets-tokopedia-lite/v2/zeus/kratos/4fede911.svg']")).size()+"");
+            item.setTotalRating(ratingElement.findElement(By.tagName("span")).getText().replace("(","").replace(")",""));
+            //System.out.println(item.getRating());
+            item.setImageUrl(webElement.findElement(By.tagName("img")).getAttribute("src"));
+            itemList.add(item);
+
 
             //scroll every 2 row of item
             if(idx%10==0){
-                js.executeScript("window.scrollBy(0,1000)");
+                js.executeScript("window.scrollBy(0,675)");
             }
 
         }
+        return itemList;
+    }
+
+    public static void writeCsvFromBean(Path path,List<Item> itemList) throws Exception {
+        Writer writer  = new FileWriter(path.toString());
+
+        StatefulBeanToCsv sbc = new StatefulBeanToCsvBuilder(writer)
+                .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
+                .build();
+
+
+        sbc.write(itemList);
+        writer.close();
     }
 }
--- src/main/java/Model/Item.java
+++ src/main/java/Model/Item.java
@@ -5,6 +5,63 @@ public class Item {
     String imageUrl;
     String price;
     String rating;
+    String totalRating;
     String sellerName;
+    String sellerLocation;
 
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getTotalRating() {
+        return totalRating;
+    }
+
+    public void setTotalRating(String totalRating) {
+        this.totalRating = totalRating;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public String getSellerLocation() {
+        return sellerLocation;
+    }
+
+    public void setSellerLocation(String sellerLocation) {
+        this.sellerLocation = sellerLocation;
+    }
 }
